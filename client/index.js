da3832fc580b6d3e1c0a1a599a153aa9425c4816
Bradley Karmin <bkarmin718@gmail.com>
2025-01-30 14:48:02 -0500

    added interactivity

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+currentLoveItem = 0;
+
 window.onload = () => {
 
   const balloonContainer = document.getElementById("balloon-container");
@@ -19,14 +21,14 @@ window.onload = () => {
     //    background-color: rgba(${r},${g},${b},0.7);
     // color: rgba(${r},${g},${b},0.9); 
     return `
-
+  
     
     box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
     margin: ${mt}px 0 0 ${ml}px;
     animation: float ${dur}s ease-in-out forwards
     `;
   }
-
+  
   function createBalloons(num) {
     for (var i = num; i > 0; i--) {
       var balloon = document.createElement("div");
@@ -37,14 +39,16 @@ window.onload = () => {
     }
   }
 
-  createBalloons(15)
-
-  function removeBalloons() {
-      if (!balloonContainer) return;
-      balloonContainer.style.opacity = '0';
-      setTimeout(() => {
-          balloonContainer.remove()
-      }, 500)
+  createBalloons(15);
+  
+  const revealButton = document.getElementById("reveal-button");
+  revealButton.onclick = () => {
+    loveItems = document.getElementsByClassName("love-item");
+    loveItems[currentLoveItem].style["display"] = "block";
+    currentLoveItem += 1;
+    if (currentLoveItem == loveItems.length) {
+      revealButton.style["display"] = "none";
+    }
   }
 }
 
